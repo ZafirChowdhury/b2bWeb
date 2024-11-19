d85ab0e7bb6f72668f49eb0172836a1dbc7c6e1b
ZafirChowdhury <zafirchowdhury69@gmail.com>
2024-11-19 22:22:26 +0600

    Add Image Upload

--- image.py
+++ image.py
@@ -3,14 +3,18 @@ import requests
 
 url = "https://api.imgbb.com/1/upload"
 api_key = "37c8ea51af6c6c2e9bf543566c763169"
+expiration = "2624016" # 1 month = 2624016 secs
 
 def upload_image_to_imgbb(image_path):
     with open(image_path, "rb") as file:
         payload = {
             "key": api_key,
-            "image": file.read(),
+            "image": base64.b16encode(file.read()),
+            "expiration" : expiration
         }
+    
     response = requests.post(url, payload)
+    
     if response.status_code == 200:
         return response.json()['data']['url']
     else:
--- sql/schema.sql
+++ sql/schema.sql
@@ -11,9 +11,9 @@ CREATE TABLE listing (
     user_id INT,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(6000),
-    
+    image_url VARCHAR(512)
     PRIMARY KEY (listing_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE 
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 
