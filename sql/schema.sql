6c5d5eea1e04ea84bebcf5323f16a1a54d9a9a7a
ZafirChowdhury <zafirchowdhury69@gmail.com>
2024-11-20 19:24:48 +0600

    Add Database Schemas

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,17 +3,44 @@ CREATE TABLE users (
     user_name VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
+    location VARCHAR(255),
+    phone_number VARCHAR(13),
+    user_image_link VARCHAR(512),
     PRIMARY KEY (user_id)
 );
 
-CREATE TABLE listing (
+CREATE TABLE listings (
     listing_id INT NOT NULL AUTO_INCREMENT,
     user_id INT,
     title VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    sold BOOLEAN NOT NULL DEFAULT FALSE,
     description VARCHAR(6000),
-    image_url VARCHAR(512)
+    image_url VARCHAR(512),
+    date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    auction_end_time TIMESTAMP,
     PRIMARY KEY (listing_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE comments (
+    comment_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT,
+    listing_id INT,
+    date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE SET NULL,
+    FOREIGN KEY (listing_id) REFERENCES listings(listing_id) ON DELETE CASCADE
+);
+
+CREATE TABLE profile_reviews (
+    review_id INT NOT NULL AUTO_INCREMENT,
+    made_by_user_id INT,
+    for_user_id INT,
+    date_posted TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (review_id),
+    FOREIGN KEY (made_by_user_id) REFERENCES users(user_id) ON DELETE SET NULL,
+    FOREIGN KEY (for_user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
 
 
